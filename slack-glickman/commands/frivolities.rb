1ba08665d363c330cb71f74f0c812e5d9e2d434e
David Thyresson <david@stattleship.com>
2016-01-11 20:43:22 -0500

    Implemented curry and fixed batpig

--- slack-glickman/commands/frivolities.rb
+++ slack-glickman/commands/frivolities.rb
@@ -3,11 +3,27 @@ module SlackGlickman
     module Commands
       class Frivolities < SlackRubyBot::Commands::Base
         command ":batpig:" do |client, data, _match|
-          send_gif(client, data.channel, 'french bulldog pup')
+          send_gif(client, data.channel, 'french bulldog')
+        end
+
+        command ":curry:" do |client, data, _match|
+          query_params = Stattleship::Params::BasketballGameLogsParams.new
+
+          query_params.player_id = 'nba-stephen-curry'
+
+          game_logs = Stattleship::BasketballGameLogs.fetch(params: query_params)
+
+          msg = game_logs.sample.to_sentence
+
+          send_message client, data.channel, ":curry: #{msg}"
         end
 
         command ":eggplant:" do |client, data, _match|
           send_gif(client, data.channel, 'brett favre')
+        end
+
+        command ":game_die:" do |client, data, _match|
+          # TODO - totally random
         end
 
         command ":pizza:" do |client, data, _match|
