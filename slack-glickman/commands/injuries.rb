c114c1bf8715d0542a24906936691335a3f4d3d3
David Thyresson <david@stattleship.com>
2016-01-12 22:47:14 -0500

    Fetch an injury for a given teamoji

--- slack-glickman/commands/injuries.rb
+++ slack-glickman/commands/injuries.rb
@@ -7,17 +7,28 @@ module SlackGlickman
                     else
                       sport
                     end
-
         command ":face_with_head_bandage: :#{statmoji}:" do |client, data, _match|
-
-          query_params = const_get("Stattleship::Params::#{sport.capitalize}InjuriesParams").new
-
-          injuries = const_get("Stattleship::#{sport.capitalize}Injuries").fetch(params: query_params)
-
-          msg = injuries.sample.note
-
+          msg = fetch_injury(sport: sport)
           send_message client, data.channel, ":face_with_head_bandage: #{msg}"
         end
+      end
+
+      SlackGlickman::App.instance.teamoji.each do |sport|
+        sport['teams'].each do |team|
+          command ":face_with_head_bandage: #{team['emoji']['default']} #{sport['emoji']}" do |client, data, _match|
+            msg = fetch_injury(sport: sport['sport'], team_id: team['slug'])
+            send_message client, data.channel, ":face_with_head_bandage: #{team['emoji']['default']} #{sport['emoji']} #{msg}"
+          end
+        end
+      end
+
+      def self.fetch_injury(sport: 'basketball', team_id:)
+        query_params = const_get("Stattleship::Params::#{sport.capitalize}InjuriesParams").new
+        query_params.team_id = team_id
+
+        injuries = const_get("Stattleship::#{sport.capitalize}Injuries").fetch(params: query_params)
+
+        msg = injuries.sample.note
       end
     end
   end
