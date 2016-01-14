55d9075226835c8cc3656c9e6bb2bb918e01a34b
David Thyresson <david@stattleship.com>
2016-01-13 22:01:10 -0500

    CHange tips bulb to be a regex

--- slack-glickman/commands/tips.rb
+++ slack-glickman/commands/tips.rb
@@ -1,7 +1,7 @@
  module SlackRubyBot
   module Commands
     class Tips < Base
-      match(':bulb:')
+      match(/^:bulb:$/)
 
       def self.call(client, data, _match)
         send_message client, data.channel, ":bulb: ProTip!"
