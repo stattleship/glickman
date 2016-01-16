c989b136c056ccbef2e65202e116b6c4928c2ac0
David Thyresson <david@stattleship.com>
2016-01-15 20:58:43 -0500

    Added colors command to return colors for sport as well as a single team

--- slack-glickman.rb
+++ slack-glickman.rb
@@ -5,6 +5,7 @@ require 'slack-glickman/data/cache'
 require 'slack-glickman/app'
 
 require 'slack-glickman/commands/base_command'
+require 'slack-glickman/commands/colors'
 require 'slack-glickman/commands/injuries'
 require 'slack-glickman/commands/feats'
 require 'slack-glickman/commands/frivolities'
---
+++ slack-glickman/commands/colors.rb
@@ -0,0 +1,37 @@
+module SlackGlickman
+  module Commands
+    class Colors < SlackGlickman::Commands::BaseCommand
+      SlackGlickman::App.instance.teamoji.each do |sport|
+        command ":lower_left_paintbrush: #{sport['emoji']}" do |client, data, _match|
+          team_colors = teams(sport: sport['sport']).map { |team| "#{team.name} #{team_hex_colors(team: team)}" }
+          send_message client, data, team_colors.join("\n")
+        end
+      end
+
+      SlackGlickman::App.instance.teamoji.each do |sportmoji|
+        sportmoji['teams'].each do |team|
+          sport = sportmoji['sport']
+          statmoji = sportmoji['emoji']
+          teamoji = team['emoji']['default']
+
+          command ":lower_left_paintbrush: #{teamoji} #{statmoji}" do |client, data, _match|
+            requested_team = teams(sport: sport).detect { |t| t.slug == team['slug'] }
+            team_colors = "#{requested_team.name} #{team_hex_colors(team: requested_team)}"
+            send_message client, data, team_colors
+          end
+        end
+      end
+
+      def self.teams(sport: 'basketball')
+        query_params = const_get("Stattleship::Params::#{sport.capitalize}TeamsParams").new
+        const_get("Stattleship::#{sport.capitalize}Teams").fetch(params: query_params)
+      end
+
+      def self.team_hex_colors(team:)
+        team.colors.
+         map { |color| "##{color}" }.
+         join(' ')
+      end
+    end
+  end
+end
