c9d1799d2f6b610d8dabc3851426e76416af1b90
David Thyresson <david@stattleship.com>
2016-01-26 13:09:14 -0500

    Adds penalty and scoring play commands

---
+++ slack-glickman/commands/penalties.rb
@@ -0,0 +1,31 @@
+module SlackGlickman
+  module Commands
+    class Penalties < SlackGlickman::Commands::BaseCommand
+      SlackGlickman::App.instance.teamoji.each do |sport|
+        sport['teams'].each do |team|
+          command ":package: #{team['emoji']['default']} #{sport['emoji']}" do |client, data, _match|
+            send_message client, data, penalties_for_team(team_id: team['slug'],
+                                                       sport: sport['sport'])
+          end
+        end
+      end
+
+      def self.penalties_for_team(team_id:, sport:)
+        query_params = const_get("Stattleship::Params::#{sport.capitalize}PenaltiesParams").new
+        query_params.team_id = team_id
+        query_params.since = '1 week ago'
+
+        if sport == 'football' then
+          query_params.interval_type = 'conferencechampionships'
+        end
+
+        penalties = const_get("Stattleship::#{sport.capitalize}Penalties").fetch(params: query_params).
+                      take(5)
+
+        msg = penalties.map(&:to_sentence).join("\n")
+
+        ":package: Go to box. Feel shame.\n#{msg}"
+      end
+    end
+  end
+end
---
+++ slack-glickman/commands/scoring_plays.rb
@@ -0,0 +1,37 @@
+module SlackGlickman
+  module Commands
+    class ScoringPlays < SlackGlickman::Commands::BaseCommand
+      SlackGlickman::App.instance.teamoji.each do |sport|
+        sport['teams'].each do |team|
+          command ":tada: #{team['emoji']['default']} #{sport['emoji']}" do |client, data, _match|
+            send_message client, data, penalties_for_team(team_id: team['slug'],
+                                                       sport: sport['sport'])
+          end
+        end
+      end
+
+      def self.penalties_for_team(team_id:, sport:)
+        query_params = const_get("Stattleship::Params::#{sport.capitalize}ScoringPlaysParams").new
+        query_params.team_id = team_id
+        query_params.since = '1 week ago'
+
+        if sport == 'football' then
+          query_params.interval_type = 'conferencechampionships'
+        end
+
+        scoring_plays = const_get("Stattleship::#{sport.capitalize}ScoringPlays").fetch(params: query_params).
+                          sample(5)
+
+        msg = scoring_plays.map(&:to_sentence).join("\n")
+
+        caption = if sport == 'hockey'
+                    'Goals scored!'
+                  else
+                    'Scoring!'
+                  end
+
+        ":tada: #{caption}\n#{msg}"
+      end
+    end
+  end
+end
